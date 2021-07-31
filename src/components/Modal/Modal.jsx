78bcc97c30c2860eec31a749b53a2bfb3c8d2582
Axeliriya <axeliriya@mail.ru>
2021-07-31 20:57:53 +0200

    add formula

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,22 +1,23 @@
 import { useSelector } from 'react-redux';
 import { productsSelectors } from '../../redux/products';
-import { getProducts } from '../../redux/products/products-operations';
-import formula from './Formula';
+// import { getProducts } from '../../redux/products/products-operations';
+// import { formula } from './Formula';
 import Loader from '../Loader';
 import styles from './Modal.module.scss';
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+// import { useEffect } from 'react';
+// import { useDispatch } from 'react-redux';
 const shortid = require('shortid');
 
-export default function Modal({ active, setActive }) {
+export default function Modal({ active, setActive, calories }) {
   const isLoading = useSelector(productsSelectors.isLoading);
-  const dispath = useDispatch();
+  // const dispath = useDispatch();
 
-  useEffect(() => {
-    const { bloodGroup } = JSON.parse(localStorage.getItem('user'));
-    const fetchProducts = () => dispath(getProducts(bloodGroup));
-    fetchProducts();
-  }, [dispath]);
+  // useEffect(() => {
+  //   const { bloodGroup } = JSON.parse(localStorage.getItem('user'));
+  //   console.log(bloodGroup);
+  //   const fetchProducts = () => dispath(getProducts(bloodGroup));
+  //   fetchProducts();
+  // }, [dispath]);
 
   const products = useSelector(productsSelectors.getNotAllowedProducts);
   const buttonClose = () => {
@@ -41,7 +42,7 @@ export default function Modal({ active, setActive }) {
             Ваша рекомендуемая суточная норма калорий составляет
           </h2>
           <p className={styles.modalData}>
-            {formula}
+            {calories}
             <span className={styles.madalCalories}>ккал</span>
           </p>
           <div className={styles.modalLine}></div>
